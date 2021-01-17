8269369c4d74f910487fb7819ed4c48124a546f5
Laxit Shahi <shahilax@my.yorku.ca>
2021-01-16 19:55:35 -0500

    bs4 setup

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import requests
 
-app = Flask(__name__)
 
-@app.route('/') # Rerpites to original website if /____ doesnt exist
-def index():
-    return render_template("website.html")
+# app = Flask(__name__)
+
+#Beautiful Soup
+bc = '029000020764'
+URL = 'https://www.walmart.com/search/?query=' + bc
+page = requests.get(URL)
+print(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+price = soup.find(class_="price-group")
+print(price)
+
+#Run the app
+# @app.route('/') # Rerpites to original website if /____ doesnt exist
+# def index():
+#     return render_template("website.html")
     
-if __name__ == "__main__":
-    app.run(debug=True, host="localhost")
-
+# if __name__ == "__main__":
+#     app.run(debug=True, host="localhost")
 
