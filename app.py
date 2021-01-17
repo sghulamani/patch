5b4840ed0ddb463ecd065735bbbad0f8dd0ccf2f
Sani <65928582+sghulamani@users.noreply.github.com>
2021-01-17 17:21:07 -0600

    testing selenium

--- app.py
+++ app.py
@@ -1,21 +1,42 @@
 from flask import Flask, render_template
+import time
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options
 
-
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from fake_useragent import UserAgent
 # app = Flask(__name__)
 
-#Beautiful Soup
+chrome_options = Options()
+ua = UserAgent(use_cache_server = False, verify_ssl=False)
+userAgent = ua.random
+print(userAgent)
+chrome_options.add_argument(f'user-agent={userAgent}')
+#chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=chrome_options)
 bc = '029000020764'
 URL = 'https://www.walmart.com/search/?query=' + bc
-page = requests.get(URL)
-print(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
+driver.get(URL)
+timeout = 30
+try:
+    time.sleep(0.25)
+    element_present = EC.presence_of_element_located((By.ID, 'global-search-input'))
+    WebDriverWait(driver, timeout).until(element_present)
+except TimeoutException:
+    print("Timed out")
 
-price = soup.find(class_="price-group")
-print(price)
+soup = BeautifulSoup(driver.page_source,"html.parser")
+driver.quit()
+for item in soup.findAll("span", {"class": "visuallyhidden"}):
+    print(item)
 
-#Run the app
+# #Run the app
 # @app.route('/') # Rerpites to original website if /____ doesnt exist
 # def index():
 #     return render_template("website.html")
